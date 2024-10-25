beb34076bc12f3ef0d9bbfd3ed8e21e0914397e9
COMPU-S035\daw1 <Juan David>
2024-10-25 10:01:09 +0200

    primer avance p p t

---
+++ vscode/Piedra, papel y tijera/Piedra, papel y tijera/src/App.java
@@ -0,0 +1,11 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("****Bienvenido al juego: Piedra, papel o tijera****");
+        System.out.println("¡¡Pulse una tecla para empezar!!");
+
+        
+
+    }
+}
