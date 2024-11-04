a44937e4ea5e3e4573aab8a8ecd4e8d977789f7e
SERVELLÓN\famil <servellonrejasjuandavid@gmail.com>
2024-11-04 07:20:34 +0100

    avance ppt

--- vscode/Piedra, papel y tijera/Piedra, papel y tijera/src/App.java
+++ vscode/Piedra, papel y tijera/Piedra, papel y tijera/src/App.java
@@ -1,10 +1,46 @@
-
+import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("****Bienvenido al juego: Piedra, papel o tijera****");
-        System.out.println("¡¡Pulse una tecla para empezar!!");
 
+        Scanner teclado = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("****Bienvenido al juego: Piedra, papel o tijera****");
+        System.out.println("En que consiste:\n El juego de Piedra, Papel o Tijera es un clásico juego de estrategia y azar que se juega con\n las manos. Consiste en elegir uno de tres elementos: piedra, papel o tijera, y compararlos para determinar quién gana.");
+
+        String opcion = "";
+        String opcionUsuario = Scanner.nexLine();
+        String opcionComputadora = random.nexLine(P,L,T);
+
+        do{
+            teclado = Scanner(System.in);
+            System.out.println("Escoje");
+            System.out.println("P - Piedra");
+            System.out.println("L - Papel");
+            System.out.println("T - Tijera");
+            System.out.println("S - Salir");
+            String [] opciones = {"Piedra, Papel, Tijera"};
+
+            switch (opcion) {
+                case "P":
+                teclado = Scanner(System.in);
+                if (opcionUsuario.equals(P)>opcionComputadora){
+                    System.out.println("Haz ganado");
+                }else{
+                    System.out.println("Haz perdido");
+                }
+                    break;
+            
+                default:
+                    break;
+            }
+
+
+
+
+        }while(!opcion.equals("S"));
+        
         
 
     }
