52af721b47bd4b0c486e30c0de6b8645a0e0b6e6
COMPU-S035\daw1 <Juan David>
2024-11-13 10:17:49 +0100

    avance dawbank

--- dawbank/dawbank/src/CuentaBancaria.java
+++ dawbank/dawbank/src/CuentaBancaria.java
@@ -1,13 +1,55 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CuentaBancaria {
-    private String datos;
+
     private String iban;
     private String titular;
     private double saldo;
-    private String ingreso;
-    private String retirada;
-    private String movimientos;
+    private Movimiento [] movimientos;
 
-    public CuentaBancaria(String datos, String iban, String titular, double saldo){
+    public CuentaBancaria( String iban, String titular, double saldo){
+
+        this.iban = iban;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        Pattern pat = Pattern.compile("[A-Z]{2}\\d[0-9]{22}$");
+        Matcher mat = pat.matcher(iban);
+        if(mat.matches() == true){
+            System.out.println("¡CORRECTO!");
+            this.iban = iban;
+        }else{
+            System.out.println("Intentelo otra vez");
+        }
 
     }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getMovimientos() {
+        return movimientos;
+    }
+
+
 }
--- dawbank/dawbank/src/movimiento.java
+++ dawbank/dawbank/src/movimiento.java
@@ -1,30 +1,40 @@
 
 import java.time.LocalDateTime;
-
-public class movimiento {
+public class Movimiento {
     //atributos
     private int id;
     private String fecha;
     private String tipo;
     private double cantidad;
-
     //constructores
-    public movimiento (String tipo, String cantidad){
+    public Movimiento (String tipo, String cantidad){
         this.id = id;
         this.fecha = LocalDateTime.now().toString();
         this.tipo = tipo; //ingreso o retirada
+        this.cantidad = 0.0;
+}
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public double getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(double cantidad) {
         this.cantidad = cantidad;
-    }
-    public int getId(){
-        return this.id;
-    }
-    public String getFecha(){
-        return this.fecha;
-    }
-    public String getTipo(){
-        return this.tipo;
-    }
-    public String getCantidad(){
-        return this.cantidad;
     }
 }
